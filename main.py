3f96d102ac154c2d88793289edbe58c0bd7ac19f
cedric <demuelenaere.cedric@gmail.com>
2025-10-21 11:09:51 +0200

    create base model of a car

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pybullet as p
 import pybullet_data
 import time
@@ -21,18 +20,46 @@ p.setGravity(0, 0, -9.81)
 
 plane_id = p.loadURDF("plane.urdf")
 
-cube_static = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5])
-cube_static_body = p.createMultiBody(
-    baseMass=0,
-    baseCollisionShapeIndex=cube_static,
-    basePosition=[0, 0, 0.5]
+car_body_half_extents = [1.0, 0.5, 0.05]
+wheel_half_extents = [0.2, 0.2, 0.2]
+
+car_body_collision = p.createCollisionShape(
+    p.GEOM_BOX, halfExtents=car_body_half_extents
+)
+car_body_visual = p.createVisualShape(
+    p.GEOM_BOX, halfExtents=car_body_half_extents, rgbaColor=[0.8, 0.1, 0.1, 1]
 )
 
-cube_dynamic = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5])
-cube_dynamic_body = p.createMultiBody(
-    baseMass=1,
-    baseCollisionShapeIndex=cube_dynamic,
-    basePosition=[0, 0, 3]
+wheel_collision = p.createCollisionShape(
+    p.GEOM_BOX, halfExtents=wheel_half_extents
+)
+wheel_visual = p.createVisualShape(
+    p.GEOM_BOX, halfExtents=wheel_half_extents, rgbaColor=[0.1, 0.1, 0.1, 1]
+)
+
+wheel_offsets = [
+    [0.8, 0.45, -0.25],   # Front-right
+    [0.8, -0.45, -0.25],  # Front-left
+    [-0.8, 0.45, -0.25],  # Rear-right
+    [-0.8, -0.45, -0.25], # Rear-left
+]
+
+car_body = p.createMultiBody(
+    baseMass=2,
+    baseCollisionShapeIndex=car_body_collision,
+    baseVisualShapeIndex=car_body_visual,
+    basePosition=[0, 0, 0.3],
+    baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
+    linkMasses=[0.5] * 4,
+    linkCollisionShapeIndices=[wheel_collision] * 4,
+    linkVisualShapeIndices=[wheel_visual] * 4,
+    linkPositions=wheel_offsets,
+    linkOrientations=[p.getQuaternionFromEuler([0, 0, 0])] * 4,
+    linkInertialFramePositions=[[0, 0, 0]] * 4,
+    linkInertialFrameOrientations=[p.getQuaternionFromEuler([0, 0, 0])] * 4,
+    linkParentIndices=[0, 0, 0, 0],
+    linkJointTypes=[p.JOINT_FIXED] * 4,
+    linkJointAxis=[[0, 0, 0]] * 4,
 )
 
 for _ in range(240 * 5):
